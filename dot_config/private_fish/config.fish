b57358563f56cf53c4c0c603069b842150d674de
Edward Amor <edward.amor3@gmail.com>
2021-04-14 17:40:16 -0400

    feat: add pyenv configuration in config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,1 +1,7 @@
+# remove default fish greeting
 set -g fish_greeting
+
+# pyenv configuration
+set -x PATH ~/.pyenv/bin $PATH
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
